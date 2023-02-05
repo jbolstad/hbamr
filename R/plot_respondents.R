815bcc36ee65829a8d99ba55cb83b995d347bd06
jbolstad <j.boelstad@gmail.com>
2023-02-05 12:11:40 +0100

    Add seed to plot_respondents

--- R/plot_respondents.R
+++ R/plot_respondents.R
@@ -10,11 +10,12 @@
 #' @param fill Fill color for density plots.
 #' @param alpha_color Number in \[0,1\]: Inverse level of transparency for line color.
 #' @param alpha_fill Number in \[0,1\]: Inverse level of transparency for fill color.
+#' @param seed A positive integer specifying an optional seed for reproducibility. The seed is used to select respondent position draws for illustrating uncertainty.
 #' @return A `ggplot` object.
 #'
 
-plot_respondents <- function(object, inc_stimuli = TRUE, n_draws = 25, color = "#053061", fill = "#2166AC", alpha_color = 0.4, alpha_fill = 0.03) {
-  pd <- get_plot_data(object, n_draws = n_draws)
+plot_respondents <- function(object, inc_stimuli = TRUE, n_draws = 25, color = "#053061", fill = "#2166AC", alpha_color = 0.4, alpha_fill = 0.03, seed = 1) {
+  pd <- get_plot_data(object, n_draws = n_draws, seed = seed)
   suppressWarnings(
   if (inc_stimuli == T) {
     p <- ggplot2::ggplot() + ggplot2::geom_density(data = pd$chi_draws, ggplot2::aes(.data$chi, by = .data$draw_no), color = ggplot2::alpha(color, alpha_color), fill = ggplot2::alpha(fill, alpha_fill)) +
