296c5159d950870c74c233acb75268d274a03822
jbolstad <j.boelstad@gmail.com>
2024-02-06 16:08:22 +0100

    Set HBAM to default for show_code()

--- R/show_code.R
+++ R/show_code.R
@@ -3,13 +3,13 @@
 #' Show the Stan code for one of the models in the package.
 #'
 #' @export
-#' @param model Character: Name of the model. See the documentation for the `hbam()` function for a list of the available models.
+#' @param model Character: Name of the model to show. Defaults to "HBAM". See the documentation for the `hbam()` function for a list of the available models.
 #' @return The function prints Stan code.
 #'
 #' @examples
 #' show_code("HBAM")
 
-show_code <- function(model = NULL) {
+show_code <- function(model = "HBAM") {
   if(!model %in% names(stanmodels)) {
     stop(paste(model, "is not a valid model name."))
   }
